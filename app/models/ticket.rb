f9c3d59b9a6bd42501ced4d1a50b39e460a3092b
Cuslar <cuslar@miuandes.cl>
2019-04-04 22:43:14 -0300

    Lab4Uslar

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,18 @@
 class Ticket < ApplicationRecord
   belongs_to :ticket_type
   belongs_to :order
+  before_create :cannot_after_start
+  before_create :cannot_after_start1
+
+	def cannot_after_start
+	  if self.start_day < Date.today
+	end
+    
+   end
+
+	def cannot_after_start1
+	  if self.start_day < Date.today
+	end
+
+    end
 end
